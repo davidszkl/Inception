8d8e1412288902d6607e406e5d74468d0f2ac6c2
davidszkl <david.szklarzewski@gmail.com>
2022-02-21 15:55:47 +0100

    php not working

--- requirements/mariadb/tools/script.sh
+++ requirements/mariadb/tools/script.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-service mysql start &
-if mysqladmin -u root --wait=2 ping; then
-    mysql -u root -h localhost -e "CREATE DATABASE $MYSQL_DB;
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
-    CREATE USER  '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PWD';
-    GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';
-    FLUSH PRIVILEGES;
-"
-mysqladmin -u root -p$MYSQL_ROOT_PWD -h localhost shutdown
-mysqld_safe
+if ! test -f /var/lib/mysql/.mysql_init; then
+    service mysql start &
+    if mysqladmin -u root -h localhost --wait=2 ping; then
+        mysql -u root -h localhost -e "CREATE DATABASE $MYSQL_DB;
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
+        CREATE USER  '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PWD';
+        GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';
+        FLUSH PRIVILEGES;
+        "
+        mysqladmin -h localhost -u root -p$MYSQL_ROOT_PWD shutdown
+        touch /var/lib/mysql/.mysql_init
+    fi
 fi
+mysqld_safe
--- requirements/wordpress/tools/script.sh
+++ requirements/wordpress/tools/script.sh
@@ -1,4 +1,9 @@
-sleep 10
-wp core install --allow-root --url='http://localhost' --title="Title" --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL
-wp user create --allow-root  wpuser wpuser@example.com --role=author --user_pass="$WP_USER_PWD"
+#!bin/sh
+if ! test -f var/www/wordpress/wp-config.php; then
+    sleep 10
+    mv wp-config.php /var/www/wordpress;
+    wp core download --path=/var/www/wordpress
+    wp core install --path=/var/www/wordpress --allow-root --url='http://localhost' --title="Title" --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL
+    wp user create --path=/var/www/wordpress --allow-root  wpuser wpuser@example.com --role=author --user_pass="$WP_USER_PWD"
+fi
 php-fpm7 --nodaemonize
