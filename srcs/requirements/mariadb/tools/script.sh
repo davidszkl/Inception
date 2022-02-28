f9d168ffd8d4d3492efcd6c6ba99b50d821880c6
davidszkl <david.szklarzewski@gmail.com>
2022-02-28 13:12:51 +0100

    save

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,18 +4,16 @@ service mysql start
 
 if [ ! -d /var/lib/mysql/wordpress_db ] ; then
 
-	mysql --no-defaults -u root -e "use mysql;						\
-		update user SET PASSWORD=PASSWORD(\"$MARIADB_ROOT_PWD\")	\
-		WHERE USER='root';"
-
-	mysql --no-defaults -u root -p"$MARIADB_ROOT_PWD" -e "			\
-		CREATE DATABASE wordpress_db;								\
-		GRANT ALL ON wordpress_db.* TO 'wpuser'@'%'					\
-		IDENTIFIED BY \"$MARIADB_WPUSER_PWD\" WITH GRANT OPTION;	\
-		FLUSH PRIVILEGES;"
-
+mysql --no-defaults -u root -e "
+	CREATE DATABASE $WP_DATABASE;
+	ALTER USER 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PWD';
+	CREATE USER '$MARIADB_WPUSER'@'%' IDENTIFIED BY '$MARIADB_WPUSER_PWD';
+	GRANT ALL PRIVILEGES ON $WP_DATABASE TO $MARIADB_WPUSERA'%';
+	FLUSH PRIVILEGES;
+	"
 fi
 
 service mysql stop
 
-exec mysqld -u root --datadir="/var/lib/mysql"
+exec mysqld -u root 
+#--datadir="/var/lib/mysql"
